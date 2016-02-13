77cea4ffc07a8ceb222e4fbfaa088c1cdbee4b44
Harrison Walters <famishedmammal@gmail.com>
2016-02-12 19:59:03 -0800

    Create Genetic_Manager.java

---
+++ Genetic_Manager.java
@@ -0,0 +1,25 @@
+public class Genetic_Manager {
+
+	//Types
+	//0 = naive AI
+	//1 = aggressive AI
+	//2 = randomized AI_smart
+	
+	int tests_perRound = 30;
+	float[][] roundRatios = {{1.0f, 0.0f, 0.0f},
+							{0.0f, 1.0f, 0.0f},
+							{1.0f, 0.0f, 1.0f},
+							{0.3f, 0.3f, 0.3f},
+							{0.5f, 0.0f, 0.5f},
+							{0.0f, 0.5f, 0.5f},
+							{0.5f, 0.5f, 0.0f}};
+	
+	public Genetic_Manager() {
+		
+	}
+	
+	public void roundOver() {
+		
+	}
+	
+}
